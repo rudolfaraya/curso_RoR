108f88b10a1e11f4cdef957cfd2808316c14afdc
CodePicnic Exporter <gitexporter@codepicnic.com>
2016-08-31 13:35:35 +0000

    First commit

---
+++ facilito2/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  
+  devise_for :users, controllers:{
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
+  
+  post "/custom_sign_up", to: "users/omniauth_callbacks#custom_sign_up"
+  
+  root 'main#home'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
